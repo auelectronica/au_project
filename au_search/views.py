d4988e609c306f1e753520bb014e58574ab35fed
Manuel Limones <limonesu.me@gmail.com>
2017-03-31 00:51:27 -0600

    Add none paginated search funtionality

--- au_search/views.py
+++ au_search/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from au_search.models import Part
 
 # Create your views here.
 def home_view(request):
     return render(request, 'au_search/home.html')
 
 def search_view(request):
-    return render(request, 'au_search/search.html')
+    keyword = request.GET.get('keyword','')
+    if keyword != '':
+        found_parts = Part.objects.filter(part_id__startswith=keyword)[:10]
+        total_found = Part.objects.filter(part_id__startswith=keyword).count()
+        context_dic = {
+                'keyword': keyword,
+                'found_parts': found_parts,
+                'total_found': total_found,
+                }
+        return render(request, 'au_search/search.html', context=context_dic)
+    else:
+        return redirect('/')
